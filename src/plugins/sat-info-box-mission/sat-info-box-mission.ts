9ecfa63372d47686880afb23197034d100679bed
Theodore Kruczek <theodore.kruczek@gmail.com>
2026-02-06 07:01:27 -0500

    refactor: :recycle: rename class to SatInfoBoxMission

--- src/plugins/sat-info-box-mission/sat-info-box-mission.ts
+++ src/plugins/sat-info-box-mission/sat-info-box-mission.ts
@@ -1,7 +1,7 @@
 import { t7e } from '@app/locales/keys';
 import { KeepTrackPlugin } from '../../engine/plugins/base-plugin';
 
-export class SatInfoBoxSponsor extends KeepTrackPlugin {
+export class SatInfoBoxMission extends KeepTrackPlugin {
   init() {
     console.error(t7e('plugins.proOnlyMsg'));
   }
