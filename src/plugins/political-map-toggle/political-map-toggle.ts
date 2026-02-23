fd3165aa19db33c90c5f070d5bc58cd08e0c0127
Theodore Kruczek <theodore.kruczek@gmail.com>
2026-02-23 09:04:49 -0500

    feat(political-map-toggle): :sparkles: add political map toggle plugin

---
+++ src/plugins/political-map-toggle/political-map-toggle.ts
@@ -0,0 +1,57 @@
+import { SoundNames } from '@app/engine/audio/sounds';
+import { ServiceLocator } from '@app/engine/core/service-locator';
+import { KeepTrackPlugin } from '@app/engine/plugins/base-plugin';
+import { IBottomIconConfig, ICommandPaletteCommand, IconPlacement, IKeyboardShortcut, UtilityGroup } from '@app/engine/plugins/core/plugin-capabilities';
+import mapImage from '@public/img/icons/map.png';
+
+export class PoliticalMapToggle extends KeepTrackPlugin {
+  readonly id = 'PoliticalMapToggle';
+  dependencies_ = [];
+
+  bottomIconCallback = (): void => {
+    this.onBottomIconClick();
+  };
+
+  getBottomIconConfig(): IBottomIconConfig {
+    return {
+      elementName: 'political-map-toggle-bottom-icon',
+      label: 'Political Map',
+      image: mapImage,
+      placement: IconPlacement.UTILITY_ONLY,
+      utilityGroup: UtilityGroup.LAYER_TOGGLE,
+    };
+  }
+
+  getKeyboardShortcuts(): IKeyboardShortcut[] {
+    return [
+      {
+        key: 'L',
+        callback: () => this.bottomMenuClicked(),
+      },
+    ];
+  }
+
+  getCommandPaletteCommands(): ICommandPaletteCommand[] {
+    return [
+      {
+        id: 'PoliticalMapToggle.toggle',
+        label: 'Toggle Political Boundaries',
+        category: 'Display',
+        shortcutHint: 'L',
+        callback: () => this.bottomMenuClicked(),
+      },
+    ];
+  }
+
+  onBottomIconClick(): void {
+    if (this.isMenuButtonActive) {
+      ServiceLocator.getSoundManager()?.play(SoundNames.TOGGLE_ON);
+      settingsManager.isDrawPoliticalMap = true;
+      this.setBottomIconToSelected();
+    } else {
+      ServiceLocator.getSoundManager()?.play(SoundNames.TOGGLE_OFF);
+      settingsManager.isDrawPoliticalMap = false;
+      this.setBottomIconToUnselected();
+    }
+  }
+}
