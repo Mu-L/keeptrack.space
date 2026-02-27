481a02a413aa0fd79a04e3bfd8e18775567c526d
Theodore Kruczek <theodore.kruczek@gmail.com>
2026-02-27 18:10:59 -0500

    chore(celestrak): :wrench: disable WatchlistFilterPlugin in config

--- configs/celestrak/settingsOverride.js
+++ configs/celestrak/settingsOverride.js
@@ -64,6 +64,9 @@ const settingsOverride = {
     WatchlistPlugin: {
       enabled: false,
     },
+    WatchlistFilterPlugin: {
+      enabled: false,
+    },
     WatchlistOverlay: {
       enabled: false,
     },
